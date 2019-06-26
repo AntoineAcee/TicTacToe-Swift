2d7aa03ee6c32c496f263bcbfbc4f391cfc9116e
Antoine Lefevre <antoine.lefevre@supinternet.fr>
2019-06-26 11:24:24 +0200

    init tic tac toe project

---
+++ TicTacToe/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  TicTacToe
+//
+//  Created by Antoine Lefevre on 21/06/19.
+//  Copyright © 2019 Antoine Lefevre All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
